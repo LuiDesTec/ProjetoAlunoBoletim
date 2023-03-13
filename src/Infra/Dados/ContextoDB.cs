018090014e6f73fca52b77b0eed72219b09f12da
Luiz Carlos <luidestec@outlook.com>
2023-03-13 00:08:45 -0300

    Migration teste com db mysql

---
+++ src/Infra/Dados/ContextoDB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ProjetoEducar.Infra.Dados
+{
+    public class ContextoDB : DbContext
+    {
+        public ContextoDB(DbContextOptions<ContextoDB> options) : base(options)
+        {
+        }
+
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoEducar.Infra.Dados;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string mySqlConnection =
+              builder.Configuration.GetConnectionString("EducandarioData");
+
+builder.Services.AddDbContextPool<ContextoDB>(options =>
+                options.UseMySql(mySqlConnection,
+                      ServerVersion.AutoDetect(mySqlConnection)));
+
+ 
+ 
+  
+ 
+ 
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,28 +34,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
