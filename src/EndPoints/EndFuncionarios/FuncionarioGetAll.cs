3e78721c0426895845ae9b97208c46b45fa7f869
Luiz Carlos <luidestec@outlook.com>
2023-06-16 02:47:11 -0300

    Ajustando conexão do BD com Dapper

--- src/EndPoints/EndFuncionarios/FuncionarioGetAll.cs
+++ src/EndPoints/EndFuncionarios/FuncionarioGetAll.cs
@@ -11,22 +11,19 @@ public class FuncionarioGetAll
     public static string[] Methods => new string[] { HttpMethods.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int page, int rows, IConfiguration configuration)
+    public static IResult Action( int page, int rows, IConfiguration configuration)
     {
-        var db = new MySqlConnection(configuration["EducandarioData"]);
+        var db = new MySqlConnection(configuration.GetConnectionString("EducandarioData"));
         var funcionarios = db.Query<FuncionarioResponse>
         (
           @"select Email, ClaimValue as Nome
-            from aspnetusers u 
-            inner join aspnetuserclaims c 
-            on u.id = c.UserId and ClaimType = 'Nome'
+            from AspNetUsers u 
+            inner join AspnetUserClaims c             
+            on u.id = c.UserId and claimtype = 'Nome'
             order by nome"
-
+            
         );
 
         return Results.Ok( funcionarios );
     }
-   
-
-
 }
